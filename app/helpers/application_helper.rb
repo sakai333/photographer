144f492f1a77ff78d379f2ee46512ab214d7a610
Sakai Kenta <sakai827yshw@gmail.com>
2021-01-18 18:05:07 +0900

    header作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページタイトル作成
+  MAIN_TITLE = "Photographer".freeze
+  def full_title(page_title = '')
+    if page_title.blank?
+      MAIN_TITLE
+    else
+      "#{page_title} - #{MAIN_TITLE}"
+    end
+  end
 end
