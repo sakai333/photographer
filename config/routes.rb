3004ca49b36a75a5485c31ebb216057145d28337
Sakai Kenta <sakai827yshw@gmail.com>
2021-02-03 22:17:59 +0900

    20210203 ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
-  get  '/about',    to: 'static_pages#about'
+  root   'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :topics
 end
