97ac1a64b6445652d83a2ddf21ea5954da5feff5
Sakai Kenta <sakai827yshw@gmail.com>
2021-02-09 20:49:36 +0900

    20210209 postsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root   'static_pages#home'
   get    '/about',   to: 'static_pages#about'
   get    '/signup',  to: 'users#new'
@@ -8,5 +6,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :posts
   resources :topics
 end
