78d2c0ab63606610356ad4598b97242542d98142
Sakai Kenta <sakai827yshw@gmail.com>
2021-03-08 16:25:37 +0900

    20210308 いいね機能ajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,14 @@
 class LikesController < ApplicationController
+  before_action :set_like
+  
   def create
     current_user.likes.create(post_id: params[:post_id])
-    redirect_to root_path
   end
   def destroy
     Like.find_by(post_id: params[:post_id], user_id: current_user.id).destroy
-    redirect_to root_path
+  end
+  private
+  def set_like
+    @post = Post.find(params[:post_id])
   end
 end
