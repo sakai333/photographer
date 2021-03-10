587d2caf37ceb7158e66bae4779bcddbc1fb66e9
Sakai Kenta <sakai827yshw@gmail.com>
2021-03-10 22:59:52 +0900

    20210310 フォロー機能の実装

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+class RelationshipsController < ApplicationController
+  def follow
+    @user = User.find(params[:id])
+    current_user.follow(@user)
+    render :follow
+  end
+  
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.unfollow(@user)
+    render :follow
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,28 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships                               
   accepts_nested_attributes_for :genre_users, allow_destroy: true
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
