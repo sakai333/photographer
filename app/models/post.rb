3f7edfa9d212552e6dd17f8692bc04aa8bc1b8a3
Sakai Kenta <sakai827yshw@gmail.com>
2021-03-10 21:19:42 +0900

    20210310 like機能修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,8 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def liked_by?(user)
-    likes.where(user_id: user.id).exists?
+    if user
+      likes.where(user_id: user.id).exists?
+    end
   end
 end
