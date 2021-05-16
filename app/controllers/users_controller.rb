0f08914bfa6c47059c2b30ffaf0e988212d5b921
Sakai Kenta <sakai827yshw@gmail.com>
2021-05-16 13:33:19 +0900

    20210516 コンテスト集計アクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy]
+  before_action :timepass,only:[:show]
 
   def show
     @user = User.find(params[:id])
