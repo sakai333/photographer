995feefc48e7514a22af5c0417db131efa3c8224
Sakai Kenta <sakai827yshw@gmail.com>
2021-05-20 22:08:15 +0900

    20210520 dockerfile修正

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,8 @@ threads threads_count, threads_count
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+# environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 # pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -38,12 +39,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "unix:///myapp/tmp/sockets/puma.sock"
-stdout_redirect "/myapp/log/puma.stdout.log", "/myapp/log/puma.stderr.log", true
+# bind "unix:///myapp/tmp/sockets/puma.sock"
+# stdout_redirect "/myapp/log/puma.stdout.log", "/myapp/log/puma.stderr.log", true
 
 
 
 # app_root = File.expand_path("../..", __FILE__)
 # bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
 # stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+stdout_redirect "#{Rails.root}/log/puma.stdout.log", "#{Rails.root}/log/puma.stderr.log", true
